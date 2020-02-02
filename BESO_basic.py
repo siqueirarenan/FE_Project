6d30612bd197854d78860f5070f77dd8c0208d32
siqueirarenan <59806712+siqueirarenan@users.noreply.github.com>
2020-02-02 01:48:28 +0100

    Initial commit

---
+++ BESO_basic.py
@@ -0,0 +1,100 @@
+"""General 3D topology optimization code by Zhihao Zuo and Yimin Xie. Note that the CAE file shall contain a model 'Model-1' with a dependent part 'Part-1' and a static step 'Step-1'."""
+import math,customKernel
+from abaqus import getInput,getInputs
+from odbAccess import openOdb
+## Function of formatting Abaqus model for stiffness optimisation
+def fmtMdb(Mdb):
+    mdl = Mdb.models['Model-1']
+    part = mdl.parts['Part-1']
+    # Build sections and assign solid section
+    mdl.Material('Material01').Elastic(((1.0, 0.3), ))
+    mdl.HomogeneousSolidSection('sldSec','Material01')
+    mdl.Material('Material02').Elastic(((0.001**3, 0.3), ))
+    mdl.HomogeneousSolidSection('voidSec','Material02')
+    part.SectionAssignment(part.Set('ss',part.elements),'sldSec')
+    # Define output request
+    mdl.FieldOutputRequest('SEDensity','Step-1',variables=('ELEDEN', ))
+    mdl.HistoryOutputRequest('ExtWork','Step-1',variables=('ALLWK', ))
+## Function of running FEA for raw sensitivities and objective function
+def FEA(Iter,Mdb,Xe,Ae):
+    Mdb.Job('Design_Job'+str(Iter),'Model-1').submit()
+    Mdb.jobs['Design_Job'+str(Iter)].waitForCompletion()
+    opdb = openOdb('Design_Job'+str(Iter)+'.odb')
+    seng = opdb.steps['Step-1'].frames[-1].fieldOutputs['ESEDEN'].values
+    for en in seng: Ae[en.elementLabel]=en.data/Xe[en.elementLabel]
+    obj=opdb.steps['Step-1'].historyRegions['Assembly ASSEMBLY'].historyOutputs['ALLWK'].data[-1][1]
+    opdb.close()
+    return obj
+## Function of preparing filter map (Fm={elm1:[[el1,el2,...],[wf1,wf2,...]],...})
+def preFlt(Rmin,Elmts,Nds,Fm):
+    # Calculate element centre coordinates
+    c0 = {}
+    for el in Elmts:
+        nds = el.connectivity
+        c0[el.label]=[sum([Nds[nd].coordinates[i]/len(nds) for nd in nds]) for i in range(3)]
+    # Weighting factors
+    for el in Elmts:
+        Fm[el.label] = [[],[]]
+        for em in Elmts:
+            dis=math.sqrt(sum([(c0[el.label][i]-c0[em.label][i])**2 for i in range(3)]))
+            if dis<Rmin:
+                Fm[el.label][0].append(em.label)
+                Fm[el.label][1].append(Rmin - dis)
+        sm = sum(Fm[el.label][1])
+        for i in range(len(Fm[el.label][0])): Fm[el.label][1][i] /= sm
+## Function of filtering sensitivities
+def fltAe(Ae,Fm):
+    raw = Ae.copy()
+    for el in Fm.keys():
+        Ae[el] = 0.0
+        for i in range(len(Fm[el][0])): Ae[el]+=raw[Fm[el][0][i]]*Fm[el][1][i]
+## Function of optimality update for design variables and Abaqus model
+def BESO(Vf,Xe,Ae,Part,Elmts):
+    lo, hi = min(Ae.values()), max(Ae.values())
+    tv = Vf*len(Elmts)
+    while (hi-lo)/hi > 1.0e-5:
+        th = (lo+hi)/2.0
+        for key in Xe.keys(): Xe[key] = 1.0 if Ae[key]>th else 0.001
+        if sum(Xe.values())-tv>0: lo = th
+        else: hi = th
+    # Label elements as solid or void
+    vlb, slb = [], []
+    for el in Elmts:
+        if Xe[el.label] == 1.0: slb.append(el.label)
+        else: vlb.append(el.label)
+    # Assign solid and void elements to each section
+    Part.SectionAssignment(Part.SetFromElementLabels('ss',slb),'sldSec')
+    Part.SectionAssignment(Part.SetFromElementLabels('vs',vlb),'voidSec')
+## ====== MAIN PROGRAM ======
+if __name__ == '__main__':
+    # Set parameters and inputs
+    pars = (('VolFrac:','0.5'), ('Rmin:', '1'), ('ER:', '0.02'))
+    vf,rmin,ert = [float(k) if k!=None else 0 for k in getInputs(pars,dialogTitle='Parameters')]
+    if vf<=0 or rmin<0 or ert<=0: sys.exit()
+    mddb = openMdb(getInput('Input CAE file:',default='D:\Doutorado\107_IZEO 3D\01_Pyton_BESO_programs\Example_bar.cae'))
+    # Design initialization
+    fmtMdb(mddb)
+    part = mddb.models['Model-1'].parts['Part-1']
+    elmts, nds = part.elements, part.nodes
+    oh, vh = [], []
+    xe, ae, oae, fm = {}, {}, {}, {}
+    for el in elmts: xe[el.label] = 1.0
+    if rmin>0: preFlt(rmin,elmts,nds,fm)
+    # Optimisation iteration
+    change, iter, obj = 1, -1, 0
+    while change > 0.001:
+        iter += 1
+        # Run FEA
+        oh.append(FEA(iter,mddb,xe,ae))
+        # Process sensitivities
+        if rmin>0: fltAe(ae,fm)
+        if iter > 0: ae=dict([(k,(ae[k]+oae[k])/2.0) for k in ae.keys()])
+        oae = ae.copy()
+        # BESO optimisation
+        vh.append(sum(xe.values())/len(xe))
+        nv = max(vf,vh[-1]*(1.0-ert))
+        BESO(nv,xe,ae,part,elmts)
+        if iter>10: change=math.fabs((sum(oh[iter-4:iter+1])-sum(oh[iter-9:iter-4]))/sum(oh[iter-9:iter-4]))
+    # Save results
+    mddb.customData.History = {'vol':vh,'obj':oh}
+    mddb.saveAs('Final_design.cae')
---
+++ FE.py
@@ -0,0 +1,68 @@
+
+
+#mdb = openMdb(path+file) 'C:\Users\sique\Desktop\QLK_Model-614-2.cae'
+def openMdb(file_name): #function to open a model data base
+    pass
+
+
+class Mdb:   #class with all the input information in a file (model data base)
+    models = {} #dictionary containing the different models in the file {'model_name':obj_from_class_mdl}
+    jobs = {}
+    def __init__(self):
+        self.models['Model-1'] = Model() #iniciation of a mdb creates a standard model
+    def saveAs(self,file_name):
+        pass
+    def Job(self,job_name,model_name): #function to crete a new model
+        new_Job = job(job_name)
+        return new_Job
+    def openStep(self,file_name):
+        self.models[self.models.keys()[0]].acis = pass #Importing step model!!!
+
+class Model:  #class of a model containing all the parts, material properties,... of a mdb
+    name = None #name of the model
+    parts = {} #dictionary containing all the different parts {'part_name':obj_from_class_part}
+    materials = {} #dictionary containing material objects {'material_name':obj_from_class_mat}
+    acis = None #geometry file saved in the model to be imported
+    def __init__(self,model_name='Model-1'):
+        self.name = model_name
+    def PartFromGeometryFile(self,name='Part-1',geometryFile=acis):  #fuction to create a new part
+        self.parts[name] = Part(name)
+        #transformation from the CAD-file
+        pass
+    def Material(self,mat_name): #function to create a new material model
+        self.materials[mat_name] = material(mat_name)
+        pass
+
+
+class Part:  #class of parts of a model
+    name = None
+    nodes = {}
+    elements = {}
+    sets = {}
+    sections ={}
+    def __init__(self,name):
+        self.name = name
+
+class Set(Part):
+    pass
+
+class Element:
+    pass
+
+class nodes:
+    pass
+
+class section:
+    pass
+
+class material:  #class of materials of a model
+    #como acessar os dados do material
+    def __init__(self,mat_name):
+        self.name = mat_name
+    def Elastic(self,dependencies=1,table=((0, 0,), )):
+
+class job:  #class of jobs of a model
+    def __init__(self,job_name):
+        self.name = job_name
+    def submit(self):  #function that runs the job
+        pass
